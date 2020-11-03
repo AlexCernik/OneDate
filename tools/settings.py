ab061df54f9691e901b7d413f4c7f350cbf54f14
lac617a <botlacrita617@gmail.com>
2020-11-03 13:10:17 -0500

    modified tools-settings added db

--- tools/settings.py
+++ tools/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config
-from . import db
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,7 +81,7 @@ WSGI_APPLICATION = 'tools.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+from . import db
 DATABASES = db.POSTGRES_LOCAL
 
 # Password validation
