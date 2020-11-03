707121fa8f8a0000854e2ed6c36622bdb6e708a4
lac617a <botlacrita617@gmail.com>
2020-11-03 13:03:04 -0500

    modified requirement.txt a requirements.txt

--- tools/wsgi.py
+++ tools/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tools.settings')
 
 application = get_wsgi_application()
+
+from decouple import config
+if config('DJANGO_PRODUCTION', default=False,cast=bool):
+  from whitenoise.django import DjangoWhiteNoise
+  application = DjangoWhiteNoise(application)
