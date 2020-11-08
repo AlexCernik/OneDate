3b097204377be3ef8b44a26bdd46705ad77769d9
lac617a <botlacrita617@gmail.com>
2020-11-07 22:05:16 -0500

    creando la pagina oficial

--- api/urls.py
+++ api/urls.py
@@ -4,14 +4,6 @@ from .api import Character_APIView,Character_APIView_Detail
 app_name = 'Api'
 
 urlpatterns = [
-  path('api/',Character_APIView.as_view(),name='api'),
-  path('api/character/<int:pk>/',Character_APIView_Detail.as_view(),name='character'),
-  re_path(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
-  # path('api/',characterList),
-  # path('characters/<int:pk>/',characterDetail),
+  path('',Character_APIView.as_view(),name='api'),
+  path('character/<int:pk>/',Character_APIView_Detail.as_view(),name='character'),
 ]
-
-# urlpatterns = [
-#   path('api/',CharacterAPI.as_view(),name='character'),
-#   path('api-auth/', include('rest_framework.urls'))
-# ]
--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,8 @@
-from django.urls import path, re_path
-# from .api import CharacterList, CharacterDetail
-# from .views import *
+from django.urls import path
+from . import views
 
-app_name = 'core'
+app_name = 'Core'
 
-# urlpatterns = [
-#   path('api/', Character_APIView.as_view()),
-#   path('api/character/<int:pk>', Character_APIView_Detail.as_view()),
-# ]
-
-# urlpatterns = [
-#   path('', index, name='index'),
-#   path('api/',api, name='api'),
-#   re_path(r'^character/(?P<pk>[0-9]+)/$', CharacterDetail.as_view(), name='character'),
-# ]
+urlpatterns = [
+  path('',views.index, name='home'),
+]
