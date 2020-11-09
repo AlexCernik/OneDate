8dd130e9813f98d68ed6a37292826ed4318b3e92
lac617a <botlacrita617@gmail.com>
2020-11-09 09:13:25 -0500

    added DropBoxStorage

--- tools/settings_production.py
+++ tools/settings_production.py
@@ -4,7 +4,7 @@ from . import db
 DATABASES = db.POSTGRES_PRODUCTION
 
 # Dropbox for image
-DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropboxStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN = config('DROPBOX_TOKEN')
 DROPBOX_CONSUMER_KEY = config('DROPBOX_KEY')
 DROPBOX_CONSUMER_SECRET = config('DROPBOX_SECRET')
