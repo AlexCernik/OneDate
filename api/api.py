692b17d945266bbc1cf21a0acecdf68a6ab37cf5
lac617a <botlacrita617@gmail.com>
2020-11-09 20:33:02 -0500

    added dropbox

--- api/api.py
+++ api/api.py
@@ -7,6 +7,27 @@ from rest_framework.decorators import api_view
 
 from .models import Character
 
+serializer_class = CharacterSerializer
+
+def get_object(pk):
+  try:
+    return Character.objects.get(pk=pk)
+  except Character.DoesNotExist:
+    raise Http404
+
+# por id
+@api_view(['GET'])
+def get_characters_details(request,pk,format='json'):
+  character = get_object(pk)
+  serializer = serializer_class(character)
+  return JsonResponse(serializer.data)
+
+@api_view(['GET'])
+def get_characters_lists(request, format=None, *args, **kwargs):
+  character = Character.objects.all()
+  serializer = serializer_class(character,many=True)
+  return Response(serializer.data)
+
 # @api_view(['GET'])
 # def apiOverView(request):
 #   api_urls = {
@@ -30,47 +51,47 @@ from .models import Character
 #   serializer = CharacterSerializer(character,many=False)
 #   return Response(serializer.data)
 
-class Character_APIView(APIView):
-  """ API VIEW DE PRUEBA """
-  serializer_class = CharacterSerializer
+# class Character_APIView(APIView):
+#   """ API VIEW DE PRUEBA """
+#   serializer_class = CharacterSerializer
 
-  def get(self, request, fortmat=None, *args, **kwargs):
-    character = Character.objects.all()
-    serializer = self.serializer_class(character,many=True)
+#   def get(self, request, fortmat=None, *args, **kwargs):
+#     character = Character.objects.all()
+#     serializer = self.serializer_class(character,many=True)
 
-    return Response(serializer.data)
+#     return Response(serializer.data)
 
-  def character(self,request, format=None):
-    serializer = self.serializer_class(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data,status=status.HTTP_201_CREATED)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#   def character(self,request, format=None):
+#     serializer = self.serializer_class(data=request.data)
+#     if serializer.is_valid():
+#       serializer.save()
+#       return Response(serializer.data,status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-class Character_APIView_Detail(APIView):
-  """ API VIEW DE PRUEBA """
-  serializer_class = CharacterSerializer
+# class Character_APIView_Detail(APIView):
+#   """ API VIEW DE PRUEBA """
+#   serializer_class = CharacterSerializer
 
-  def get_object(self,pk):
-    try:
-      return Character.objects.get(pk=pk)
-    except Character.DoesNotExist:
-      raise Http404
+#   def get_object(self,pk):
+#     try:
+#       return Character.objects.get(pk=pk)
+#     except Character.DoesNotExist:
+#       raise Http404
 
-  def get(self,request,pk,format=None):
-    character = self.get_object(pk)
-    serializer = self.serializer_class(character)
-    return Response(serializer.data)
+#   def get(self,request,pk,format=None):
+#     character = self.get_object(pk)
+#     serializer = self.serializer_class(character)
+#     return Response(serializer.data)
 
-  def put(self,request,pk,format=None):
-    character = self.get_object(pk)
-    serializer = self.serializer_class(character,data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+#   def put(self,request,pk,format=None):
+#     character = self.get_object(pk)
+#     serializer = self.serializer_class(character,data=request.data)
+#     if serializer.is_valid():
+#       serializer.save()
+#       return Response(serializer.data)
+#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-  def delete(self,request,pk,format=None):
-    character = self.get_object(pk)
-    character.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+#   def delete(self,request,pk,format=None):
+#     character = self.get_object(pk)
+#     character.delete()
+#     return Response(status=status.HTTP_204_NO_CONTENT)
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include, re_path
-from .api import Character_APIView,Character_APIView_Detail
+# from .api import Character_APIView,Character_APIView_Detail
+from .api import get_characters_details, get_characters_lists
 
 app_name = 'Api'
 
 urlpatterns = [
-  path('',Character_APIView.as_view(),name='api'),
-  path('character/<int:pk>/',Character_APIView_Detail.as_view(),name='character'),
+  path('',get_characters_lists,name='api'),
+  path('character/<int:pk>/',get_characters_details,name='character'),
 ]
