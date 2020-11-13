1cbe523e9d655d473a95927c33c05fa5e51c3bd4
lac617a <botlacrita617@gmail.com>
2020-11-13 11:48:09 -0500

    CORS a settings

--- tools/settings.py
+++ tools/settings.py
@@ -64,11 +64,12 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS  = [
     'https://apionepiece.herokuapp.com',
-    'https://www.dropbox.com',
     'http://localhost:8080',
     'http://localhost:5501',
+    'http://localhost:3000',
     'http://127.0.0.1:8000',
     'http://127.0.0.1:5501',
+    'http://127.0.0.1:3000',
 ]
 # CORS_ALLOW_METHODS = [
 #     'GET',
