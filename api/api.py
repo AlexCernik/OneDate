09f646067861ea317508d9646980693bfc21ea17
lac617a <botlacrita617@gmail.com>
2020-11-20 14:45:52 -0500

    mejorando los packages y logica

--- api/api.py
+++ api/api.py
@@ -1,11 +1,7 @@
-from rest_framework import status
-from rest_framework.views import APIView
-from .serializers import CharacterSerializer
 from rest_framework import generics
-from rest_framework.response import Response
-from django.core import serializers
+from .serializers import CharacterSerializer
+from django.http import JsonResponse, Http404
 from rest_framework.renderers import JSONRenderer
-from django.http import JsonResponse, Http404,HttpResponse
 from rest_framework.decorators import api_view, renderer_classes
 
 from .models import Character
@@ -39,6 +35,7 @@ class Get_characters_all(generics.ListAPIView):
   serializer_class = CharacterSerializer
   renderer_classes = [JSONRenderer]
 
+# buscando por los personajes que esten vivos
 @api_view(['GET'])
 def get_characters_alive(request,format=None):
   try:
@@ -48,8 +45,9 @@ def get_characters_alive(request,format=None):
       return JsonResponse(serializer.data, safe=False)
     return JsonResponse({"Error":"DoesNotExist"})
   except Character.DoesNotExist:
-    return Http404
+    raise Http404
 
+# buscando por los personajes que esten muertos
 @api_view(['GET'])
 def get_characters_dead(request,format=None):
   try:
@@ -59,49 +57,4 @@ def get_characters_dead(request,format=None):
       return JsonResponse(serializer.data, safe=False)
     return JsonResponse({"Error":"DoesNotExist"})
   except Character.DoesNotExist:
-    return Http404
-
-# class Character_APIView(APIView):
-#   """ API VIEW DE PRUEBA """
-#   serializer_class = CharacterSerializer
-
-#   def get(self, request, fortmat=None, *args, **kwargs):
-#     character = Character.objects.all()
-#     serializer = self.serializer_class(character,many=True)
-
-#     return Response(serializer.data)
-
-#   def character(self,request, format=None):
-#     serializer = self.serializer_class(data=request.data)
-#     if serializer.is_valid():
-#       serializer.save()
-#       return Response(serializer.data,status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-# class Character_APIView_Detail(APIView):
-#   """ API VIEW DE PRUEBA """
-#   serializer_class = CharacterSerializer
-
-#   def get_object(self,pk):
-#     try:
-#       return Character.objects.get(pk=pk)
-#     except Character.DoesNotExist:
-#       raise Http404
-
-#   def get(self,request,pk,format=None):
-#     character = self.get_object(pk)
-#     serializer = self.serializer_class(character)
-#     return Response(serializer.data)
-
-#   def put(self,request,pk,format=None):
-#     character = self.get_object(pk)
-#     serializer = self.serializer_class(character,data=request.data)
-#     if serializer.is_valid():
-#       serializer.save()
-#       return Response(serializer.data)
-#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-#   def delete(self,request,pk,format=None):
-#     character = self.get_object(pk)
-#     character.delete()
-#     return Response(status=status.HTTP_204_NO_CONTENT)
+    raise Http404
